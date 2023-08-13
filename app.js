6609eeda064934f742abf43bc92d4ec00c86939e
2fatihceylan <49781433+2fatihceylan@users.noreply.github.com>
2023-08-13 17:34:27 +0300

    lenslight ikinci  kommit

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import conn from './db.js';
+
+dotenv.config();
+
+//connection db
+conn();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+//ejs template engine
+app.set('view engine', 'ejs');
+
+//static files middlewawre
+app.use(express.static('public'));
 
 
 app.get("/",(req,res)=>{
-    res.send("index sayfasi");
+    res.render("index");
 })
-
+app.get("/about",(req,res)=>{
+    res.render("about");
+})
 
 app.listen(port,()=>{
     console.log(`Application running on port: ${port}`);
 })
+
+
