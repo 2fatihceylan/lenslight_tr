f0a500fc791240d33b981b749dfb22ecc4954834
2fatihceylan <49781433+2fatihceylan@users.noreply.github.com>
2023-08-13 16:39:27 +0300

    lenslight ilk kommit

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+
+app.get("/",(req,res)=>{
+    res.send("index sayfasi");
+})
+
+
+app.listen(port,()=>{
+    console.log(`Application running on port: ${port}`);
+})
